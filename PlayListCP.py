92ac3c792e3f57ab46079b75c7643032c307a7b7
VectorWang2015 <vectorwang@hotmail.com>
2021-08-06 21:19:43 +0800

    Added python script

---
+++ PlayListCP.py
@@ -0,0 +1,12 @@
+import os
+import os.path
+import re
+
+def readPlayListFile(fd):
+    """read a file descriptor, return a list of lines with relative paths of songs"""
+    pass
+
+
+def processSong(song_path, new_path):
+    """parse song_path, cp the song to the new_path"""
+    pass
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ZX300PlayListCP
+
+For Device: Sony ZX300a music player
+This python script intends to read each Playlist file (M3U8), create file folder for it, copy every song in the list, and reset all the relative paths.
