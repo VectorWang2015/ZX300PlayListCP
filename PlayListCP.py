63b6914c2761d18a3150ed3bd2ca7308b2f17b3d
VectorWang2015 <vectorwang@hotmail.com>
2021-08-06 23:54:57 +0800

    First usable version

--- PlayListCP.py
+++ PlayListCP.py
@@ -4,6 +4,7 @@ import os.path
 import re
 import shutil
 
+old_dir = "./"
 root_dir = "d:/"
 
 def processSong(song_tuple, new_dir):
@@ -12,6 +13,41 @@ def processSong(song_tuple, new_dir):
     song_new_path = new_dir+'/'+song_name
     print("Copying {} ------> {}".format(song_path,song_new_path))
     # shutil.copy(song_path, song_new_path)
+    print("Done!")
+
+
+def processNewList(old_PL_path, new_PL_path, song_list):
+    """input songlist of tuples, old_PL_path, new_PL_path
+       cp playlist to new path, and change each song's relative path in the list"""
+    print("Processing new play list file {} -----> {}".format(old_PL_path, new_PL_path))
+    if os.path.exists(new_PL_path):
+        os.remove(new_PL_path)
+    shutil.copy(old_PL_path, new_PL_path)
+
+    new_PL_descriptor = open(new_PL_path, mode='r+')
+    PL_lines = new_PL_descriptor.readlines()
+    new_PL_content = ""
+    old_path, new_path = song_list[0]
+    #print(song_list)
+    index =1
+    for line in PL_lines:
+        if index > len(song_list):
+            new_PL_content+=line
+            continue
+        if line==old_path+'\n':
+            new_PL_content+=new_path+'\n'
+            try:
+                old_path, new_path = song_list[index]
+                index+=1
+            except:
+                index+=1
+        else:
+            new_PL_content+=line
+    #print(new_PL_content)
+    new_PL_descriptor.seek(0)
+    new_PL_descriptor.truncate()
+    new_PL_descriptor.write(new_PL_content)
+    print("Done!")
 
 
 def processSongList(song_list_path):
@@ -48,7 +84,7 @@ def processPlayListFile(file_name):
 
 if __name__=="__main__":
     pl_pattern = re.compile(".+\.M3U8")
-    root_file_list = os.listdir('./')
+    root_file_list = os.listdir(old_dir)
     
     for file_name in root_file_list:
         if pl_pattern.match(file_name):
@@ -65,3 +101,5 @@ if __name__=="__main__":
             #print(song_list)
             for song_tuple in song_list:
                 processSong(song_tuple, root_dir+'/'+dir_name)
+            processNewList(old_dir+file_name, root_dir+'/'+dir_name+'/'+file_name, song_list)
+
